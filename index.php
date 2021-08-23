943df54ba30b435db404a81bfa9f903c6d5332fb
Paulus Christofel S <75057796+pythpal20@users.noreply.github.com>
2021-08-23 14:19:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Mr. Kitchen</title>
+	<script>
+		window.location = 'apps/login.php';
+	</script>
+</head>
+<body>
+	<!--
+	DB Customer 2.0 Merupakan pengganti dari DB Customer sebelumnya,
+	yang berubah dalam sistem ini adalah, antar muka yang lebih userfriendly, lebih responsive
+	penataan menu dan tombol yang lebih menarik dan rapih
+	sistem dengan OOP MySqli, sebelumnya menggunakan PDO sehingga sintak PHP yang sangat panjang untuk menampilkan data.
+
+	System 		: dbcustomer 2.o
+	re-build by : Paulus Christofel Situmorang
+	tahun 		: 2021
+	-->
+</body>
+</html>
